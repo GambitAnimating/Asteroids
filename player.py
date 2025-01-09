b286dec93dee35506adc7e4426b003c9fc00e36d
GambitAnimating <lumarigamesdev@gmail.com>
2025-01-08 19:37:18 -0500

    Added multiplier for when trying to move opposite of velocity

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.shot_timer = 0
+        self.accel_timer = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
@@ -34,16 +35,44 @@ class Player(CircleShape):
         if keys[pygame.K_SPACE]:
             if self.shot_timer < 0:
                 self.shoot()
-            
-        self.move(dt)
+
+        if keys[pygame.K_w]:
+            self.thrust(dt)
+        else:
+            if self.accel_timer > 0:
+                self.accel_timer -= dt
+            else:
+                self.accel_timer = 0
+        
+        self.position += self.velocity * dt
 
         self.shot_timer -= dt
 
-    def move(self, dt):
+    def thrust(self, dt):
+        if self.accel_timer < PLAYER_SEC_TILL_MAX_ACCEL:
+            self.accel_timer += dt
+        else:
+            self.accel_timer = PLAYER_SEC_TILL_MAX_ACCEL
+            
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
+        percent_to_max_speed = self.accel_timer / PLAYER_SEC_TILL_MAX_ACCEL
+        extra_backwards_vel = 0
+        if self.velocity != pygame.Vector2(0,0):
+            velocity_dot = pygame.Vector2.dot(forward, self.velocity)
+            
+            if velocity_dot < 0:
+                extra_backwards_vel = -velocity_dot
+
+        accel_on_curve = self.ease_out_quint(percent_to_max_speed) * (PLAYER_ACCEL_SPEED_PER_SEC + extra_backwards_vel)
+        self.velocity += forward * accel_on_curve * dt
+
+        if self.velocity.magnitude() >= PLAYER_MAX_SPEED:
+            self.velocity = self.velocity.normalize() * PLAYER_MAX_SPEED
 
     def shoot(self):
         temp_shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         temp_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
         self.shot_timer = PLAYER_SHOOT_COOLDOWN
+    
+    def ease_out_quint(self, x):
+        return 1.0 - (1.0-x)**5.0
